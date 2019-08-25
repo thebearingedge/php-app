d34c4bedf98ea19c6cdc1b47a258c7f13d239071
Tim Davis <contact@timdav.is>
2019-08-25 23:50:27 +0000

    Clean up "dev.sh" script a bit.

--- dev.sh
+++ dev.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-set -a
-
-if [ ! -f ./.env ]; then
+if [ ! -f .env ]; then
   echo 'Error: ".env" file is required. Start by copying ".env.example".' 1>&2
   exit 1
 fi
 
+set -a
 source .env
-
 set +a
 
 php -S "$PHP_HOST:$PHP_PORT" -t "server/public" -c "php.ini"
