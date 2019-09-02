2659ca449a722f1ea69fd9d8b9647ccfb7a6d8e0
Tim Davis <contact@timdav.is>
2019-09-02 09:17:38 -0700

    Remove extraneous path concatenation from routing.

--- server/public/index.php
+++ server/public/index.php
@@ -1,13 +1,13 @@
 <?php
 
-require_once __DIR__ . '/../api/_lifecycle.php';
+require_once '../api/_lifecycle.php';
 
 switch ($request['path']) {
   case '/':
     readfile('index.html');
     exit;
   case '/api/todos':
-    require_once __DIR__ . "/..${request['path']}.php";
+    require_once "..${request['path']}.php";
   default:
     throw new ApiError("Cannot ${request['method']} ${request['path']}", 404);
 }
